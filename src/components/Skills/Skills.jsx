f2303f045f47fb7efcd211b69d24d9a65a3ee557
Chandan gupta <chandan1499>
2021-09-01 00:56:21 +0530

    skills logo name added

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,16 +5,46 @@ export const Skills = () => {
         <div className={styles.container} id="#skills">
             <h1 className={styles.heading}>SKILLS</h1>
             <div className={styles.iconsDiv}>
-                <div><i className="devicon-git-plain-wordmark colored technology"></i></div>
-                <div><i class="devicon-bootstrap-plain colored"></i></div>
-                <div><i class="devicon-css3-plain-wordmark colored technology"></i></div>
-                <div><i class="devicon-html5-plain-wordmark colored technology"></i></div>
-                <div><i class="devicon-javascript-plain colored technology"></i></div>
-                <div><i class="devicon-redux-original" style={{ color: "#7248B6"}}></i></div>
-                <div><i class="devicon-react-plain-wordmark colored technology"></i></div>
-                <div><i class="devicon-github-original technology"></i></div>
-                <div><i class="devicon-cplusplus-plain" style={{ color: '#00427E' }}></i></div>
-                <div><i class="devicon-materialui-plain colored"></i></div>
+                <div>
+                    <i className="devicon-git-plain-wordmark colored technology"></i><br />
+                    <div>GIT</div>
+                </div>
+                <div>
+                    <i class="devicon-bootstrap-plain colored"></i><br />
+                    <div>BOOTSTRAP</div>
+                </div>
+                <div>
+                    <i class="devicon-css3-plain-wordmark colored technology"></i><br />
+                    <div>CSS3</div>
+                </div>
+                <div>
+                    <i class="devicon-html5-plain-wordmark colored technology"></i><br />
+                    <div>HTML5</div>
+                </div>
+                <div>
+                    <i class="devicon-javascript-plain colored technology"></i><br />
+                    <div>JAVASCRIPT</div>
+                </div>
+                <div>
+                    <i class="devicon-redux-original" style={{ color: "#7248B6" }}></i><br />
+                    <div>REDUX</div>
+                </div>
+                <div>
+                    <i class="devicon-react-original colored"></i>
+                    <div>REACT</div>
+                </div>
+                <div>
+                    <i class="devicon-github-original technology"></i><br />
+                    <div>GITHUB</div>
+                </div>
+                <div>
+                    <i class="devicon-cplusplus-plain" style={{ color: '#00427E' }}></i><br />
+                    <div>C++</div>
+                </div>
+                <div>
+                    <i class="devicon-materialui-plain colored"></i><br />
+                    <div>MATERIAL UI</div>
+                </div>
             </div>
         </div>
     );
