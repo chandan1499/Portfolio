3bd72eaac519e226936185185f4298de42ceb603
Chandan gupta <chandan1499>
2021-09-02 10:34:35 +0530

    resume button added to home page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,6 +2,10 @@ import styles from './Profile.module.css';
 import profilePic from '../../images/profilePic.jpg';
 
 export const Profile = () => {
+    const handleResumeClick = () => {
+        window.open("https://drive.google.com/file/d/1wl1XICXHKQKdUUpTLFL4mjtENQt6tJqc/view");
+    }
+
     return (
         <div className={styles.container} id="#about">
             <div className={styles.aboutContainer}>
@@ -10,6 +14,7 @@ export const Profile = () => {
                         <h1>Hi, I'm <span>Chandan Gupta</span></h1>
                         <h3>Full Stack Developer</h3>
                         <p>A passionate programmer with a curious mind who loves solving problems. I have an experience of building Web applications with JavaScript / Reactjs / HTML / CSS etc. I'm currently actively looking for new opportunities</p>
+                        <button className={styles.resumeButton} onClick={handleResumeClick}>Resume</button>
                     </div>
                     <div className={styles.rightSection}>
                         <img src={profilePic} alt="" />
