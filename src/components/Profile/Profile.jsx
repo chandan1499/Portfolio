9222cb28e04af23937655c3f6ff8e08cfa37f6bd
chandan1499 <gupta.chandan1499@gmail.com>
2021-11-20 19:18:19 +0530

    profile pic changed

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import styles from './Profile.module.css';
-import profilePic from '../../images/profilePic.jpeg';
+import profilePic from '../../images/profilePic.jpg';
 
 export const Profile = () => {
     const handleResumeClick = () => {
